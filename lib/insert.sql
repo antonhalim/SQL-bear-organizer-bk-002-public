3e289b35d95e2e8c79ae3ccb687b4d2be3026c7e
axton21 <axton_n@yahoo.com>
2015-02-28 20:56:30 -0500

    done

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES
+('Mr. Chocolate', 20, 'M', 'dark brown', 'calm', 'FALSE'),
+('Rowdy', 5, 'M', 'brown', 'silly', 'TRUE'),
+('Tabitha', 16, 'F', 'dark brown', 'naughty', 'TRUE'),
+('Sergeant Brown', 22, 'M', 'black', 'arrogant', 'TRUE'),
+('Melissa', 10, 'F', 'dark navy', 'filthy', 'TRUE'),
+('Grinch', 18, 'M', 'rainbow', 'bitchy', 'TRUE'),
+('Wendy', 8, 'F', 'brown', 'sexy', 'TRUE'),
+(null, null, 'M', 'black', 'aggressive', 'TRUE');
